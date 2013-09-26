41a8c8e4cd9911aa4f74c8dbfde77a39f99fd541
Bilal Soidik <bilalsoidik@outlook.com>
2013-09-26 18:06:39 +0000

    Intégreation du sdk, recupération de données, affichage

--- src/FB/groupeBundle/Controller/AccueilController.php
+++ src/FB/groupeBundle/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 namespace FB\groupeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \BaseFacebook;
 
 class AccueilController extends Controller
 {
@@ -12,8 +13,13 @@ class AccueilController extends Controller
         return $this->render('FBgroupeBundle:Accueil:index.html.twig');
     }
     public function login_reussiAction(){
-        
-    }
+        $facebook->setExtendedAccessToken();
+        $facebook = $this->get('fos_facebook.api');
+        $moi=$facebook->api('/me');
+
+         return $this->render('FBgroupeBundle::affiche.html.twig', array('tableau'=>$moi));  
+       }
+
     public function  deconnecteAction(){
         
    }

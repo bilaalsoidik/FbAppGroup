52f979ab6c5e2e94423fca608ec3aba66937f70e
Bilal Soidik <bilalsoidik@outlook.com>
2013-09-29 14:36:28 +0000

    ajout de méthode au controleur

--- src/FB/groupeBundle/Controller/AccueilController.php
+++ src/FB/groupeBundle/Controller/AccueilController.php
@@ -3,21 +3,25 @@
 namespace FB\groupeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use \BaseFacebook;
+
 
 class AccueilController extends Controller
 {
     
     public function indexAction()
     {
+        
         return $this->render('FBgroupeBundle:Accueil:index.html.twig');
     }
     public function login_reussiAction(){
+      
+       
+       
+         $facebook = $this->get('fos_facebook.api');
+         
         $facebook->setExtendedAccessToken();
-        $facebook = $this->get('fos_facebook.api');
-        $moi=$facebook->api('/me');
-
-         return $this->render('FBgroupeBundle::affiche.html.twig', array('tableau'=>$moi));  
+        $utilisateur=$facebook->api('/me');
+         return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig', array('utilisateur'=>$utilisateur));  
        }
 
     public function  deconnecteAction(){

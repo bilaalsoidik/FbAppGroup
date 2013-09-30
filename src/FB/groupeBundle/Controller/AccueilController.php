2491a7993b9894432a5c0c0a9c58efc5ccebca81
Bilal Soidik <bilalsoidik@outlook.com>
2013-09-30 04:08:23 +0000

    Securité d'accès assuré

--- src/FB/groupeBundle/Controller/AccueilController.php
+++ src/FB/groupeBundle/Controller/AccueilController.php
@@ -3,8 +3,8 @@
 namespace FB\groupeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
-
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Cookie;
 class AccueilController extends Controller
 {
     
@@ -15,13 +15,16 @@ class AccueilController extends Controller
     }
     public function login_reussiAction(){
       
-       
-       
-         $facebook = $this->get('fos_facebook.api');
-         
+        $facebook = $this->get('fos_facebook.api'); 
+        $ancien_AK=$facebook->getAccessToken();
         $facebook->setExtendedAccessToken();
+        $access_token=$facebook->getAccessToken();
+        $temp=59*24*3600;
+        setcookie('access_token', $access_token,time()+$temp);
         $utilisateur=$facebook->api('/me');
-         return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig', array('utilisateur'=>$utilisateur));  
+        return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig', array('utilisateur'=>$utilisateur, 'ancien_AK'=>$ancien_AK));
+
+         
        }
 
     public function  deconnecteAction(){

d389b5f0bf298eff1cf4ee7182065b01fbe73eae
Bilal Soidik <bilalsoidik@outlook.com>
2013-10-03 04:48:19 +0000

    Ajout de groupe realisé

--- src/FB/groupeBundle/Controller/AccueilController.php
+++ src/FB/groupeBundle/Controller/AccueilController.php
@@ -3,7 +3,7 @@
 namespace FB\groupeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use FB\groupeBundle\Form\GroupeType;
 
 class AccueilController extends Controller
 {   
@@ -19,9 +19,40 @@ class AccueilController extends Controller
         $temp=59*24*3600;
         setcookie('accessToken', $access_token,time()+$temp);
         $utilisateur=$facebook->api('/me');
-        return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig', array('utilisateur'=>$utilisateur, 'ancien_AK'=>$ancien_AK));
+        return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig', 
+                                  array('utilisateur'=>$utilisateur, 
+                                          'afficheMessg'=>0));
        }
-
+  
+   public function ajouterGroupAction(){
+       $facebook = $this->get('fos_facebook.api');
+       //On a besoin toujours pour afficher les info de l'utilsateur courant
+       $utilisateur=$facebook->api('/me');
+       
+       $formulaire=$this->createForm(new GroupeType());
+       $em=$this->getDoctrine()->getManager();  
+       $request=$this->getRequest();
+       if($request->isMethod('POST')){
+        $formulaire->bind($request);
+        $un_groupe=$formulaire->getData();
+        $em->persist($un_groupe);
+        $em->flush();
+        return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig',
+                        array('utilisateur'=>$utilisateur,
+                               'afficheMessg'=>1,
+                               'message'=>' Le groupe a été enregistré en base de données avec scccès'));
+       }
+       return $this->render('FBgroupeBundle:FbGroupeViews:InitGroup.html.twig',
+                        array('utilisateur'=>$utilisateur,
+                               'afficheMessg'=>1,
+                               'message'=>'Il y a des erreurs'));
+   }
+   //recuperation du formulaire d'ajout de groupe
+   public function getFormGroupAction(){
+        $formulaire=$this->createForm(new GroupeType());
+        return $this->render('FBgroupeBundle:FbGroupeViews:formGroup.html.twig',
+                                array('formulaire'=>$formulaire->createView()));
+   }
     public function  deconnecteAction(){
         
    }

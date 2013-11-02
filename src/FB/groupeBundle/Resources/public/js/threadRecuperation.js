e58f79d688e44431e4d381537b5bea17a8165f9d
Bilal Soidik <bilalsoidik@outlook.com>
2013-11-02 13:13:41 +0000

    Ajout de nouvelle ressources public,le fichier .js du worker et un fichiers son

---
+++ src/FB/groupeBundle/Resources/public/js/threadRecuperation.js
@@ -0,0 +1,31 @@
+function get(url) {
+  try {
+    var xhr = new XMLHttpRequest();;
+    xhr.open('GET', url, false);
+    xhr.send();
+    return xhr.responseText;
+  } catch (e) {
+    return 'Erreur'+e;; // turn all errors into empty results
+  }
+  }
+  
+function ExecuterEncore(data){
+postMessage(data);
+console.log(data);
+ExecuterEncore(get(URL)); 
+}
+
+this.onmessage = function (event) {
+URL=event.data;
+ExecuterEncore(get(URL));
+};
+
+/*
+ * var thread = new Worker('threadRecuperation.js');
+   function postDataToThread(donnes) {
+     thread.postMessage(donnes);
+   }
+thread.onmessage = function (event) {
+     var data = event.data 
+     }
+ */
